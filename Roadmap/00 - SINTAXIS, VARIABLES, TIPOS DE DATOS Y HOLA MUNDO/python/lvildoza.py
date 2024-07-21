21f2aa72fc7ae60441f78bc4c71417adfd83bd81
Leo <67205500+lvildoza@users.noreply.github.com>
2024-07-21 18:58:40 -0300

    mis resultados

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lvildoza.py
@@ -0,0 +1,30 @@
+# https://www.python.org/
+
+# Comentarios
+# Esto es un comentario en una sola línea
+""" 
+Esto es un comentario en varias líneas
+"""
+'''
+Esto tamnbién es un comentario en varias líneas
+'''
+
+# Variables
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de la variable"
+
+# Constantes
+MY_CONSTANT = "Mi constante" # por convención
+MY_CONSTANT = "JAJSJASSJJSAJS"
+
+# Datos primitivos
+my_int = 1 # ENTEROS: se ingresa número sin comilla
+my_float = 1.5 # DECIMALES: se ingresa el dato con . y sin comillas
+my_bool = True # BOOLEANOS: Verdadero
+my_bool = False # BOOLEANOS: Falso
+my_string = "¡Hola, Python!" #STRING: Se coloca el dato entre comillas ""
+
+print("¡Hola, Python 2024!")
+print(my_string)
+print(my_float)
+print(type(my_string))
